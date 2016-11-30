a313d8391f3a3ab0667ebb5360facf646fc8360d
Ahmed Zoghayyer <Ahmed.j.z@hotmail.com>
2016-11-30 15:11:56 -0800

    I have completed the game for now

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-// $(document).ready(function() {
-// 	var count = 0;
-//    $('.box').click(function(value) {
-//        count++; 
-//     console.log($('.box').val());
-//    });
-// });
 
-// this is a comment.
-var result = document.getElementsByClassName('box');
+function decide(array) {
+	var counter = 0;
+	var sign = false;
+	var correctEnteries = [
+         [1,2,3],
+         [4,5,6],
+         [7,8,9],
+         [1,4,7],
+         [2,5,8],
+         [3,6,9],
+         [1,5,9],
+         [3,5,7]
+		]
+    correctEnteries.forEach(function(innerArray,i) {
+    	counter = 0;
+      array.forEach(function(number, j) {
+         if (innerArray.indexOf(Number(number)) !==-1) {
+             counter++;	
+         }
+      });
+      if (counter === 3) {
+      	sign = true;
+      }
+    });
+    return (sign === true) ? true : false
+}
 
-console.log(result.length);
-
-result[1].addEventListener('click', function() {
-	console.log(result.value);
-}, false);
-
+var count = 0;
+var result = [];
+var result1 = [];
+function change(ele) {
+	    count++;
+	    if (count % 2 === 0) {
+	      //decide(ele.value);
+	      result.push(ele.value);
+	      ele.value = 'O';
+	} else {
+		  //decide(ele.value);
+		  result1.push(ele.value);
+		  ele.value = 'X';
+	}
+	if (count === 9) {
+		if (decide(result1)) {
+			alert('X have Won the Game');
+		} else if (decide(result)) {
+			alert('O have Won the Game');
+		} else {
+			alert("NO ONE WON, PLEASE REFRESH THE BROWSER TO PLAY AGAIN");
+		}
+	}
+}// we cannot define function inside the document.ready cuz it does not have enough scope to be called from.
